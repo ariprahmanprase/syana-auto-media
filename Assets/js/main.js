4ca69325d9048208926381982240ad6b14a3dd87
ariprahmanprase <prasetyoarifrahman@gmail.com>
2023-05-07 22:11:53 +0700

    first code

---
+++ Assets/js/main.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".mySwiperhero", {
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+});
